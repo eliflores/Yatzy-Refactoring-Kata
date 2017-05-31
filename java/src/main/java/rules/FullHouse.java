b830e575dde14a2aef9c11a54a5ffb0a494d1c3c
elifloresch <elifloresch@gmail.com>
2017-05-31 21:18:05 +0200

    + started splitting scoring rules across different classes

---
+++ java/src/main/java/rules/FullHouse.java
@@ -0,0 +1,42 @@
+package rules;
+
+public class FullHouse implements ScoringRule {
+
+    @Override
+    public int score(int... dices) {
+        int[] tallies;
+        boolean _2 = false;
+        int i;
+        int _2_at = 0;
+        boolean _3 = false;
+        int _3_at = 0;
+
+
+        tallies = new int[6];
+        //TODO remove dup code
+        for (i = 0; i < dices.length; i++) {
+            int dice = dices[i];
+            tallies[dice - 1]++;
+
+        }
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 2) {
+                _2 = true;
+                _2_at = i + 1;
+            }
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 3) {
+                _3 = true;
+                _3_at = i + 1;
+            }
+
+        if (_2 && _3)
+            return _2_at * 2 + _3_at * 3;
+        else
+            return 0;
+
+
+    }
+}
